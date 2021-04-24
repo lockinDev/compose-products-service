79235dba1f76ddaa9e4880bd8cf55bbc7934b4ab
lockinDev <javieralmario12@gmail.com>
2021-04-24 16:45:24 -0400

    Added support for Composite-product service documentation and Swagger ui.

---
+++ product-composite-service/src/main/java/se/lockin/microservices/composite/product/config/SpringFoxConfig.java
@@ -0,0 +1,26 @@
+package se.lockin.microservices.composite.product.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import static java.util.Collections.emptyList;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+
+public class SpringFoxConfig {
+	
+	
+	
+    
+    
+
+}
--- product-service/src/main/java/se/lockin/microservices/core/product/services/ProductServiceImpl.java
+++ product-service/src/main/java/se/lockin/microservices/core/product/services/ProductServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import se.lockin.api.core.product.Product;
 import se.lockin.api.core.product.ProductService;
+import se.lockin.util.exceptions.InvalidInputException;
+import se.lockin.util.exceptions.NotFoundException;
 import se.lockin.util.http.ServiceUtil;
 
 @RestController
@@ -24,6 +26,11 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public Product getProduct(int productId) {
+		
+		if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+        if (productId == 13) throw new NotFoundException("No product found for productId: " + productId);
+		
 		return new Product(productId, String.format("name-%s", productId), 123,
 				serviceUtil.getServiceAddress());
 	}
